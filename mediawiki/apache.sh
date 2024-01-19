5719995382f1f8c2dc63f5c981f72f4d966cbebb
arjun <arjun3458@gmail.com>
2024-01-19 10:17:51 +0530

    playbook

--- mediawiki/apache.sh
+++ mediawiki/apache.sh
@@ -3,5 +3,7 @@ url="https://releases.wikimedia.org/mediawiki/1.41/mediawiki-1.41.0.tar.gz"
 curl -O $url
 
 yum install httpd -y
-cd /var/www/html
-tar -xf mediawiki-1.41.0.tar.gz
+
+cd  /var/www/html
+
+tar -xf /tmp/mediawiki-1.41.0.tar.gz
